c4e5050bc93350c23738e3ddf56ee023fc7b8656
Dhruva Bansal <dbansal36@gatech.edu>
2020-01-04 12:50:29 +0530

    Updates

---
+++ precache-manifest.5c089ace8e6e62c5aa148bde67044573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3046c12390709880bf0bd4fc8ed51e",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "f46b312f93b2604b6dc8",
+    "url": "/homepage/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "5512c8fed77090f5e365",
+    "url": "/homepage/static/js/2.a4dedd15.chunk.js"
+  },
+  {
+    "revision": "ec61a163d907f02c19419f7af635709d",
+    "url": "/homepage/static/js/2.a4dedd15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f46b312f93b2604b6dc8",
+    "url": "/homepage/static/js/main.e7f7bb9a.chunk.js"
+  },
+  {
+    "revision": "5c63e7b635bcb3198859",
+    "url": "/homepage/static/js/runtime-main.d1a9b83b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.c8ff5e7579feb111ae1196f1ac6da06b.js"
+  "/homepage/precache-manifest.5c089ace8e6e62c5aa148bde67044573.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
